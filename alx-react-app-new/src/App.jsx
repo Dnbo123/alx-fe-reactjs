bf3a533f38eeec9b73fdcaf8ab96697dd7fb184f
Don Bosco <don965877@gmail.com>
2024-08-24 02:56:58 +0300

    Added counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import UserProfile from './components/UserProfile'
 import MainContent from './components/MainContent'
 import Footer from '../alx-react-app/src/components/Flav'
+import Counter from './components/Counter'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 <UserProfile name="Don", age="18", bio="Thrive"/>
 <MainContent/>
 <Footer/>
+<Counter/>
 </>
   )
 }
