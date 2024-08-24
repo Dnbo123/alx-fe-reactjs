6341091cdadea278caf3c119a1e7fc3476f6c6c1
Don Bosco <don965877@gmail.com>
2024-08-25 02:13:29 +0300

    company website

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import React from 'react'
+
+function Navbar() {
+  return (
+<nav>
+    <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><link to="/services">Services</link></li>
+        <li><link to="/contact">Contact</link></li>
+    </ul>
+</nav>
+);
+}
+
+export default Navbar;
