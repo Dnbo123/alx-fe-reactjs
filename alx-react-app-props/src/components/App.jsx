29a4f95fa4663f00a46422e616aceca030f879de
Don Bosco <don965877@gmail.com>
2024-08-24 05:15:05 +0300

    rectified

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,14 +1,15 @@
-import UserContext from './components/UserContext';
+import React from 'react';
+import UserContext from './UserContext';
 import ProfilePage from './ProfilePage';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-<>
-<UserContext.Provider value={userData}>
+
+  return (
+    <UserContext.Provider value={userData}>
       <ProfilePage />
     </UserContext.Provider>
-</>
-  //return <ProfilePage userData={userData} />;
+  );
 }
 
 export default App;
---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserDetails from './UserDetails';
+
+function ProfilePage() {
+  return <UserDetails />;
+}
+
+export default ProfilePage;
