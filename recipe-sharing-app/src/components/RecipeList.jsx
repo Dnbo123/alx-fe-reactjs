f4dda7b0623531d4aa2c053930c34239ec818d0e
Don Bosco <don965877@gmail.com>
2024-08-25 22:58:05 +0300

    rectified

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,21 @@
 import { useRecipeStore } from './recipeStore';
 
-const RecipeList = () => {
+function RecipeList() {
   const recipes = useRecipeStore(state => state.recipes);
 
   return (
     <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h2>Recipes</h2>
+      <ul>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </ul>
     </div>
   );
-};
+}
+
+export default RecipeList;
