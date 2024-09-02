99f92eafcdb152563d3df4ba62f62392149aa6b2
Don Bosco <don965877@gmail.com>
2024-09-03 01:46:40 +0300

    added Post componen in main

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,8 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import Navigation from './NavigationComponent.jsx';
+import Navigation from './components/NavigationComponent.jsx';
+import PostsComponent from './components/PostsComponent.jsx'
+import { Component } from 'react';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +12,7 @@ function App() {
       <div className="App">
         <h1>Query Demo in React</h1>
         <Navigation />
+        <PostsComponent/>
       </div>
      
       <ReactQueryDevtools initialIsOpen={false} />
