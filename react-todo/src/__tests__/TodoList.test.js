3e1dfa7ab90d6576e85c7c233e9c675bd1b267bb
Don Bosco <don965877@gmail.com>
2024-09-03 05:31:16 +0300

    added components

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import TodoList from '../TodoList.jsx';
+import TodoList from '../ToDoList.jsx';
 
 //checks if the component renders correctly by looking for the "Todo List" heading.
 describe('TodoList Component', () => {
