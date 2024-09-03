014687b092a070dc46b4df228fea022f52a078dd
Don Bosco <don965877@gmail.com>
2024-09-03 03:51:41 +0300

    updated components

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import Home from './components/Home';
-import Profile from './components/Profile.jsx';
-import BlogPost from './components/BlogPost.jsx';
+import Profile from './components/Profile';
+import BlogPost from './components/BlogPost';
+import Login from './components/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-          </ul>
-        </nav>
+    <AuthProvider>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
+              <li><Link to="/login">Login</Link></li>
+            </ul>
+          </nav>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile/*" element={<Profile />} />
-          <Route path="/blog/:id" element={<BlogPost />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route 
+              path="/profile/*" 
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              } 
+            />
+            <Route path="/blog/:id" element={<BlogPost />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-
-// This is an authenticationmock-up check. 
-const isAuthenticated = () => {
-  return localStorage.getItem('isLoggedIn') === 'true';
-};
+import { useAuth } from '../contexts/AuthContext';
 
 function ProtectedRoute({ children }) {
+  let auth = useAuth();
   let location = useLocation();
 
-  if (!isAuthenticated()) {
+  if (!auth.user) {
     // Redirects users to the /login page, but saves the current location they were
     // trying to go to when they were redirected. This allows the app to send them
     // along to that page after they login, which is a Better U/X
