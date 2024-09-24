7ef9b87f6adcb5dafe89d0c540f7ba1988611580
Don Bosco <don965877@gmail.com>
2024-09-24 04:58:45 +0300

    iniatialized project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App-1">
+      <header className="App-header">
+        <h1>GitHub User Search Project</h1>
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App
