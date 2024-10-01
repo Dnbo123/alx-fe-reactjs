d1d1a6cabf065ddc4998f43f8b7181349c4b0333
Don Bosco <don965877@gmail.com>
2024-10-01 03:44:17 +0300

    revisited project

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,9 +8,9 @@ function UserProfile() {
         alt="User" 
         className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-1g md:text-x1 text-blue-800 my-3 sm:my-4 hover:text-blue-500">John Doe</h1>
+      <h1 className="text-lg sm:text-1g md:text-x1 text-blue-800 my-3 sm:my-4 hover:text-blue-500">User profile</h1>
       <p className="text-sm sm:text-sm md:text-xl text-gray-600">
-        Developer at Example Co. Loves to write code and explore new technologies.
+        This user loves to write code and explore new technologies.
       </p>
     </div>
   );
